c3a0a5fe91d7248232e14f3355d11184f2db63c2
mark <markjohn.ignacio@cvsu.edu.ph>
2024-11-28 15:13:54 +0800

    20

--- routes/web.php
+++ routes/web.php
@@ -62,16 +62,14 @@ Route::get('/', function (){
 });
 Route::get('/tasks', function () use ($tasks){
     return view('index', [
-       "tasks" => $tasks
+       "tasks" => \App\Models\Task::latest()
+//           ->where('complete', true)
+           ->get()
     ]);
 })->name("tasks.index");
 
-Route::get('/tasks/{id}', function ($id) use($tasks){
-    $task = collect($tasks)->firstWhere('id', $id);
-    if(!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
-
+Route::get('/tasks/{id}', function ($id){
+    $task = \App\Models\Task::findOrFail($id);
     return view('show', ['task' => $task]);
 })->name('task.show');
 
