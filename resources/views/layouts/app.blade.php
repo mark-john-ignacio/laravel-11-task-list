a371c0f7a70173c0138da197623ee734c02cd548
mark <markjohn.ignacio@cvsu.edu.ph>
2024-12-03 09:42:20 +0800

    34 Alpine js on success notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 {{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">--}}
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     <title>Laravel 11 Task List App</title>
 
     {{--blade-formatter-disable--}}
@@ -37,14 +38,33 @@
     @yield('styles')
 </head>
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
+
     <h1 class="text-2xl mb-4">
         @yield('title')
     </h1>
-    @if (session()->has('success'))
-        <div>{{ session('success') }}</div>
-    @endif
-    <div>
-        @yield('content')
+
+    <div x-data="{ flash: true }">
+        @if (session()->has('success'))
+            <div x-show="flash" class="relative mb10 rounded border border-green-40 bg-green-100 px-4 py-3 text-lg text-green-700"
+            role="alert" >
+                <strong class="font-bold">Success</strong>
+                <div>
+                    {{ session('success') }}
+                </div>
+
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                       stroke-width="1.5" @click="flash = false"
+                       stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </span>
+
+            </div>
+        @endif
+
+
+            @yield('content')
     </div>
 </body>
 </html>
